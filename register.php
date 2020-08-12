9936332f5f3590dcef1c9fab5f2d314048f18770
brainmuller77 <37088527+brainmuller77@users.noreply.github.com>
2020-08-12 04:39:27 +0000

    Update register.php

--- register.php
+++ register.php
@@ -13,7 +13,7 @@ require 'includes/form_handlers/login_handler.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->  
-    <link rel="icon" type="image/png" href="login/images/icons/favicon.ico"/>
+    <link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
 <!--===============================================================================================-->
     <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
 <!--===============================================================================================-->
@@ -180,21 +180,21 @@ require 'includes/form_handlers/login_handler.php';
     <div id="dropDownSelect1"></div>
     
 <!--===============================================================================================-->
-    <script src="login/vendor/jquery/jquery-3.2.1.min.js"></script>
+    <script src="vendor/jquery/jquery-3.2.1.min.js"></script>
 <!--===============================================================================================-->
-    <script src="login/vendor/animsition/js/animsition.min.js"></script>
+    <script src="vendor/animsition/js/animsition.min.js"></script>
 <!--===============================================================================================-->
-    <script src="login/vendor/bootstrap/js/popper.js"></script>
-    <script src="login/vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="vendor/bootstrap/js/popper.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
 <!--===============================================================================================-->
-    <script src="login/vendor/select2/select2.min.js"></script>
+    <script src="vendor/select2/select2.min.js"></script>
 <!--===============================================================================================-->
-    <script src="login/vendor/daterangepicker/moment.min.js"></script>
-    <script src="login/vendor/daterangepicker/daterangepicker.js"></script>
+    <script src="vendor/daterangepicker/moment.min.js"></script>
+    <script src="vendor/daterangepicker/daterangepicker.js"></script>
 <!--===============================================================================================-->
-    <script src="login/vendor/countdowntime/countdowntime.js"></script>
+    <script src="vendor/countdowntime/countdowntime.js"></script>
 <!--===============================================================================================-->
-    <script src="login/js/main.js"></script>
+    <script src="js/main.js"></script>
 
 </body>
 </html>
